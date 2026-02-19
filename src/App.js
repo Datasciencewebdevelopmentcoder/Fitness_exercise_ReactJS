4fd08cb1f18bbc7a1ca50eccaf57abd0ae9ae410
Kasey Nelson <123kasey456@gmail.com>
2026-02-18 19:17:36 -0600

    setup landing page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from 'react'
-
+import { Routes, Route } from 'react-router-dom'
+import {Box} from '@mui/material'
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import ExerciseDetail from './components/ExerciseDetail'
+import Footer from './components/Footer'
+import './App.css'
 const App = () => {
   return (
-    <div>App</div>
+    <Box width ='400px' sx={{width: {xl: '1488px'}}} m='auto'>
+        <Navbar/>
+        <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/exercise' element={<ExerciseDetail />} />
+        </Routes>
+        <Footer/>
+    </Box>
   )
 }
 
